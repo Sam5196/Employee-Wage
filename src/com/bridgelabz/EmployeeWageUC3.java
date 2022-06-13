581e2871c71887c28012a66b9721c8eab4a859cc
Sam5196 <sammedpachore1008@gmail.com>
2022-06-13 15:59:24 +0530

    Added:Part Time Employee Wage

--- src/com/bridgelabz/EmployeeWageUC3.java
+++ src/com/bridgelabz/EmployeeWageUC3.java
@@ -1,12 +1,13 @@
 package com.bridgelabz;
 
-public class EmployeeWageUC2 {
+public class EmployeeWageUC3 {
     static int IS_FULL_TIME = 1;
+    static int Is_PART_TIME = 2;
     static int EMP_WAGE_PER_HOUR = 20;
     public static void main(String[] args) {
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
         if (empCheck == 1)
             System.out.println("Full Time");
@@ -17,12 +18,15 @@ public class EmployeeWageUC2 {
 
         if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
+        } else if (empCheck == Is_PART_TIME) {
+            empHrs = 4;
         } else
             empHrs = 0;
 
         empWage = empHrs * EMP_WAGE_PER_HOUR;
         System.out.println("Employee Hours :" + empHrs);
         System.out.println("Employee Wage :" + empWage);
+
 
 
     }
